4dc05e390f05343aae383aa5879e1b2ca58c6850
Megan Adams <meganoliviawendyadams@gmail.com>
2019-07-09 00:39:50 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,8 @@
-def reverse_each_word(arr)
-
-arr_reversed = arr.collect {|ele| ele.reverse}
-puts arr_reversed
-
+def reverse_each_word(string)
+  array = string.split(" ") #turn string into an array
+  test_array = []
+  array.collect do|string| #iterate over the array
+    test_array << string.reverse #reverse each word in the array
+  end
+  test_array.join(" ")
 end
