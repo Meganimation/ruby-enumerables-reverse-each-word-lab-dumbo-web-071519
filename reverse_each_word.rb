30f526c30ae60e338003383880b1ee23d658e678
Megan Adams <meganoliviawendyadams@gmail.com>
2019-07-09 00:40:37 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
   arr = string.split(" ") 
   test_array = []
   arr.collect do|string| 
-    test_array << string.reverse 
+    test_array.push string.reverse 
   end
   test_array.join(" ")
 end
